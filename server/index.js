032fd1bfd324d5a688d7308814e4c15aa50a6d73
thamizhema <thamizharasan2373@gmail.com>
2023-09-23 15:47:58 +0530

    static added

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = 8000;
 
+app.use(express.static(path.join(__dirname, "..", "client", "build")));
 app.get("/", (req, res) => {
-  res.send("API is Working");
+  // res.send("API is Working");
+  res.sendFile(path.join(__dirname, "..", "client", "build"));
 });
 
 app.listen(PORT, () => {
